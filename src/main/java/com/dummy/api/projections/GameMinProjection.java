0f3d94bf1da40a800afeee15c3b11dfbce9d970a
crisaoo <cristianocosta2019@outlook.com>
2023-12-18 21:18:57 -0300

    Fix searchByList query and homolog is ready

--- src/main/java/com/dummy/api/projections/GameMinProjection.java
+++ src/main/java/com/dummy/api/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.dummy.api.projections;
 public interface GameMinProjection{
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
