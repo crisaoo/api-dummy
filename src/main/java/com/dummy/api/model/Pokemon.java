fb4ca3abcae0e13598e3b7b4d013c82f40352e87
crisaoo <cristianocosta2019@outlook.com>
2023-12-29 20:11:32 -0300

    Add tables creation file for dev profile

--- src/main/java/com/dummy/api/model/Pokemon.java
+++ src/main/java/com/dummy/api/model/Pokemon.java
@@ -6,7 +6,17 @@ import java.util.List;
 
 import com.dummy.api.model.enums.PokemonType;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
